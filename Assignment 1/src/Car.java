264fdaddea8ddaa418b471ea85e7cecad8ae19e6
04320 <alexey.ryabov@hotmail.com>
2020-02-03 12:24:25 +0100

    Skip

--- Assignment 1/src/Car.java
+++ Assignment 1/src/Car.java
@@ -1,28 +1,48 @@
-/**
- * Abstract class Car is a super class to car models.
- */
-
 import java.awt.*;
 
 public abstract class Car implements Movable{
-    public int nrDoors; // Number of doors on the car
+    public  int nrDoors; // Number of doors on the car
     public double enginePower; // Engine power of the car
     public double currentSpeed; // The current speed of the car
     public Color color; // Color of the car
     public String modelName; //The car model name
 
     final static String INV_ARG = "Value not allowed"; //
+<<<<<<< HEAD
 
     //Direction and coordinats
     public double x = 0;
     public double y = 0;
     public direction dir = direction.NORTH;
+
+    public enum direction{
+        NORTH, SOUTH, WEST, EAST
+=======
+    /**
+     * Representations of Direction and position
+     */
+    private double x = 0;
+    private double y = 0;
+    private direction dir = direction.NORTH;
     
     public enum direction{
-        NORTH, SOUTH, WEST, EAST;
+        NORTH, SOUTH, WEST, EAST
     }
 
-    //Getters
+    /**
+     * Getters
+     */
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
+    public direction getDir(){
+        return dir;
+>>>>>>> a65c3d607116eb045af59f5a9a63b9822a9d4286
+    }
+
     public int getNrDoors(){
         return nrDoors;
     }
@@ -39,12 +59,17 @@ public abstract class Car implements Movable{
         return color;
     }
 
-    //Setters
+    /**
+     * Setters
+     * @param clr which color should be set for the car
+     */
     public void setColor(Color clr){
         color = clr;
     }
 
-    //Car Components
+    /**
+     * Starts / Stops the engine (affects currentSpeed)
+     */
     public void startEngine(){
         currentSpeed = 0.1;
     }
@@ -53,6 +78,10 @@ public abstract class Car implements Movable{
         currentSpeed = 0;
     }
 
+    /**
+     * Increases the speed of the car.
+      * @param amount a value in the interval [0,1]
+     */
     public void gas(double amount) {
         if (amount >= 0 && amount <= 1) {
             double max = this.getEnginePower();
@@ -69,6 +98,11 @@ public abstract class Car implements Movable{
         }
     }
 
+    /**
+     * Decreases the speed of the car
+     * @param amount a value in the interval [0,1]
+     */
+
     public void brake(double amount) {
         if (amount >= 0 && amount <= 1){
             double min = 0;
@@ -84,6 +118,9 @@ public abstract class Car implements Movable{
         }
     }
 
+    /**
+     * Moves the car in the current direction, turns the car left or right
+     */
     @Override
     public void move() {
         if (dir == direction.SOUTH){
@@ -130,7 +167,11 @@ public abstract class Car implements Movable{
         }
     }
 
-    public void incrementSpeed(double amount){} // Is overrriden if specified in subclass
-    public void decrementSpeed(double amount){} // Is overrriden if specified in subclass
+    /**
+     * Is called from the gas/brake method.
+     * Does nothing unless a  method with the same name is also defined as Override in the car subclass
+     */
+    public void incrementSpeed(double amount){}
+    public void decrementSpeed(double amount){}
 
 }
--- Assignment 1/test/test.java
+++ Assignment 1/test/test.java
@@ -1,5 +1,4 @@
 /**
- * Is a test class.
  */
 
 import org.junit.Before;
@@ -13,14 +12,16 @@ import static junit.framework.TestCase.assertTrue;
 public class test {
     Volvo240 test1 = new Volvo240();
     Saab95 test2 = new Saab95();
-    Car test3;
 
     @Before
     public void init(){
     }
     @Test
     public void checknrDoors(){
-        assertTrue(test1.getNrDoors() == 4 && test2.getNrDoors() == 2);
+        out.println(test1.nrDoors);
+        test1.nrDoors = 5;
+        out.println(test1.nrDoors);
+        assertTrue(test1.nrDoors == 5 && test2.getNrDoors() == 2);
     }
     @Test
     public void checkColor(){
@@ -97,7 +98,7 @@ public class test {
     }
     @Test
     public void checkMove(){
-        out.println(test1.dir);
+        out.println(test1.getDir());
         test1.move();
         //out.println(test1.y);
         //out.println(test1.getCurrentSpeed());
@@ -105,18 +106,18 @@ public class test {
         test1.move();
         //out.println(test1.y);
         //out.println(test1.getCurrentSpeed());
-        assertTrue(test1.y == test1.getCurrentSpeed());
+        assertTrue(test1.getY() == test1.getCurrentSpeed());
     }
     @Test
     public void checkTurnRight(){
         test1.turnRight();
-        assertTrue(test1.dir == Car.direction.EAST);
+        assertTrue(test1.getDir() == Car.direction.EAST);
     }
     @Test
     public void checkTurnLeft(){
         test1.turnLeft();
         test1.turnLeft();
-        assertTrue(test1.dir == Car.direction.SOUTH);
+        assertTrue(test1.getDir() == Car.direction.SOUTH);
     }
     @Test
     public void checkColorChange(){
